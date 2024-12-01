2d35e0d4513c029555d449f638b0032e28481e9e
Alisson Alves <105590677+aas1989@users.noreply.github.com>
2024-12-01 11:02:04 -0300

    teste variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,22 @@
+
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String nome = "Nome: Alisson Alves";
+            System.out.println(nome);
+
+        double salarioMinino = 2500.50;
+            System.out.println(salarioMinino);
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+            System.out.println(numeroCurto);
+
+        int numero = 5;
+            System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+            System.out.println(VALOR_DE_PI);
+        
     }
 }
