b20cd92596f8f36ac1ad4bea269a445f355c9096
Alisson Alves <105590677+aas1989@users.noreply.github.com>
2024-12-14 10:16:16 -0300

    Projeto Smart

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,44 @@
+public class Operadores {
+    public static void main(String[] args) {
+        /*String nomeCompleto = "Linguagem" + "JAVA";
+        System.out.println(nomeCompleto);
+
+        int numero= 5;
+        numero = - numero;
+        System.out.println(numero);
+        numero = numero * -1;
+        System.out.println(numero);
+        numero = 5;
+        numero ++;
+        System.out.println(numero);
+        
+        int a, b;
+        a = 6;
+        b = 6;
+        String resultado = "";
+        if(a==b)
+            resultado = "Verdadeiro";
+        else
+            resultado = "Falso";  
+            
+        System.out.println(resultado);
+        
+        int a, b;
+        a = 6;
+        b = 6;
+
+        String resultado = a==b ? "Verdadeiro" : "Falso";
+        System.out.println(resultado);
+        
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+        System.out.println("numeroUm é igual a numeroDois?" + simNao);
+        simNao = numero1 != numero2;
+        System.out.println("numeroUm é diferente de numeroDois?" + simNao);
+        */
+        
+
+    }
+}
